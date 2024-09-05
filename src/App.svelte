7adc40dfaa9e45473554801f6dd6cb68ff3808e2
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2024-09-05 00:29:52 -0400

    Clean up repo, upgrade static deployment workflow

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  
-  import * as A from "@automerge/automerge/next"
   import { document as doc } from "@automerge/automerge-repo-svelte-store"
   import { IndexedDBStorageAdapter } from "@automerge/automerge-repo-storage-indexeddb"
   import { BroadcastChannelNetworkAdapter } from '@automerge/automerge-repo-network-broadcastchannel'
@@ -11,8 +7,7 @@
   import * as Tone from "tone";
   
   import { type SyncedSynth } from './lib/model'
-    import { bubble } from 'svelte/internal';
-    import { onDestroy } from 'svelte';
+  import { onDestroy } from 'svelte';
   
   const repo = new Repo({
     storage: new IndexedDBStorageAdapter("synced-synth"),
@@ -64,14 +59,6 @@
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
   <h1>Vite + Svelte</h1>
 
   <div class="card">
@@ -83,30 +70,4 @@
       <button on:click={start}>Start</button>
     {/if}
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
